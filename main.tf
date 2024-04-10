aecdbb2a353e6eaa30431cf015d56aa0bc7d109e
mshan011181 <121811920+mshan011181@users.noreply.github.com>
2024-04-10 22:35:08 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,5 +100,19 @@ resource "google_compute_instance" "vm_instance" {
         } 
 }
 
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+output "private_key" {
+  value = tls_private_key.ssh_key.private_key_pem
+  sensitive=true
+}
+
+output "public_key" {
+  value = tls_private_key.ssh_key.public_key_openssh
+  sensitive=true
+}
 
   
