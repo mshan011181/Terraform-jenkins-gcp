22374dca7728851d5e1c0e519e06829418bb5321
mshan011181 <121811920+mshan011181@users.noreply.github.com>
2024-04-29 13:48:28 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+# Define Oracle environment variables
+export ORACLE_SID=ORCLCDB
+export ORACLE_HOME=/opt/oracle/product/21c/dbhome_1
+export PATH=$ORACLE_HOME/bin:$PATH
+
+# Define SQL queries
+queries=(
+    "SELECT name from FROM v\$database;"
+    "SELECT username  FROM dba_users;"    
+)
+
+# Execute SQL queries using SQL*Plus
+for query in "${queries[@]}"; do
+    echo "Executing query: $query"
+    echo "$query" | sqlplus -S sys/Shan9871 as sysdba
+done
 
